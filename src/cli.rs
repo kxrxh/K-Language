5fe6f640d32b3e347f16e5e3c7c68c10b46c78ea
kxrxh <kxrxh.contact@gmail.com>
2023-08-09 23:02:56 +0300

    moving to PEST

---
+++ src/cli.rs
@@ -0,0 +1,59 @@
+use clap::{Parser, Subcommand};
+
+static LOGO: &str = r" ___  __                   ___       ________  ________   ________
+|\  \|\  \                |\  \     |\   __  \|\   ___  \|\   ____\
+\ \  \/  /|_  ____________\ \  \    \ \  \|\  \ \  \\ \  \ \  \___|
+ \ \   ___  \|\____________\ \  \    \ \   __  \ \  \\ \  \ \  \  ___
+  \ \  \\ \  \|____________|\ \  \____\ \  \ \  \ \  \\ \  \ \  \|\  \
+   \ \__\\ \__\              \ \_______\ \__\ \__\ \__\\ \__\ \_______\
+    \|__| \|__|               \|_______|\|__|\|__|\|__| \|__|\|_______|";
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = LOGO)]
+pub(crate) struct Args {
+    #[command(subcommand)]
+    subcommand: Command,
+}
+
+impl Args {
+    pub fn get_subcommand(&self) -> &Command {
+        &self.subcommand
+    }
+}
+
+#[derive(Subcommand, Debug)]
+pub(crate) enum Command {
+    Run(RunArgs),
+    Check,
+    Test,
+    Version,
+}
+
+impl Command {
+    pub fn run(&self) {
+        match self {
+            Command::Run(args) => {
+                // Access and use RunArgs here
+            }
+            Command::Check => {
+                // Handle Check subcommand
+            }
+            Command::Test => {
+                // Handle Test subcommand
+            }
+            Command::Version => {
+                println!(
+                    "{name} {version}",
+                    name = env!("CARGO_PKG_NAME"),
+                    version = env!("CARGO_PKG_VERSION")
+                );
+            }
+        }
+    }
+}
+
+#[derive(Debug, Parser)]
+pub struct RunArgs {
+    #[arg(help = "Input klang source code file")]
+    file: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,105 @@
-use crate::lexer::lex_error::print_error_with_line;
-use crate::lexer::lexer::lex;
+// use clap::Parser;
+// use crate::lexer::lex_error::print_error_with_line;
+// use crate::lexer::lexer::lex;
 
-mod lexer;
+// mod lexer;
+// mod cli;
 
-static LOGO: &str = r" ___  __                   ___       ________  ________   ________
-|\  \|\  \                |\  \     |\   __  \|\   ___  \|\   ____\
-\ \  \/  /|_  ____________\ \  \    \ \  \|\  \ \  \\ \  \ \  \___|
- \ \   ___  \|\____________\ \  \    \ \   __  \ \  \\ \  \ \  \  ___
-  \ \  \\ \  \|____________|\ \  \____\ \  \ \  \ \  \\ \  \ \  \|\  \
-   \ \__\\ \__\              \ \_______\ \__\ \__\ \__\\ \__\ \_______\
-    \|__| \|__|               \|_______|\|__|\|__|\|__| \|__|\|_______|";
+// fn main() {
+//     let args = cli::Args::parse();
+//     args.get_subcommand().run();
+//     // const COMMANDS: [(&str, &str); 4] = [
+//     ("RUN", "run"),
+//     ("TEST", "test"),
+//     ("VERSION", "version"),
+//     ("HELP", "help"),
+// ];
+// let max_command_width = COMMANDS
+//     .iter()
+//     .map(|(command, _)| command.len())
+//     .max()
+//     .unwrap_or(0);
+//
+// match std::env::args().nth(1) {
+//     Some(args) => {
+//         let source_code = std::fs::read_to_string(&args).unwrap();
+//         match lex(source_code.as_str()) {
+//             Ok(tokens) => {
+//                 println!("{:?}", tokens);
+//             }
+//             Err(err) => {
+//                 print_error_with_line(err);
+//             }
+//         }
+//     }
+//     None => {
+//         println!("{LOGO}");
+//         println!("Usage: klang [command] [args]\n");
+//         for (command, description) in COMMANDS.iter() {
+//             print_command_with_description(command, description, max_command_width);
+//         }
+//     }
+// }
+//}
+
+// fn print_command_with_description(command: &str, description: &str, column_width: usize) {
+//     println!(
+//         "{:<width$}{}",
+//         command,
+//         description,
+//         width = column_width + 5
+//     );
+// }
+
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct PestGrammarParser;
 
 fn main() {
-    const COMMANDS: [(&str, &str); 4] = [
-        ("RUN", "run"),
-        ("TEST", "test"),
-        ("VERSION", "version"),
-        ("HELP", "help"),
-    ];
-    let max_command_width = COMMANDS
-        .iter()
-        .map(|(command, _)| command.len())
-        .max()
-        .unwrap_or(0);
+    let input = r#"let var = "dsa""#;
 
-    match std::env::args().nth(1) {
-        Some(args) => {
-            let source_code = std::fs::read_to_string(&args).unwrap();
-            match lex(source_code.as_str()) {
-                Ok(tokens) => {
-                    println!("{:?}", tokens);
-                }
-                Err(err) => {
-                    print_error_with_line(err);
-                }
+    match PestGrammarParser::parse(Rule::program, input) {
+        Ok(pairs) => {
+            for pair in pairs {
+                process_statement(pair.into_inner());
             }
         }
-        None => {
-            println!("{LOGO}");
-            println!("Usage: klang [command] [args]\n");
-            for (command, description) in COMMANDS.iter() {
-                print_command_with_description(command, description, max_command_width);
-            }
+        Err(e) => {
+            eprintln!("Error: {}", e);
         }
     }
 }
 
-fn print_command_with_description(command: &str, description: &str, column_width: usize) {
-    println!(
-        "{:<width$}{}",
-        command,
-        description,
-        width = column_width + 5
-    );
+fn process_statement(pairs: pest::iterators::Pairs<Rule>) {
+    for pair in pairs {
+        match pair.as_rule() {
+            Rule::VAR_DECLARATION => process_var_declaration(pair.into_inner()),
+            Rule::STATEMENT => process_statement(pair.into_inner()),
+            Rule::SEMICOLON => (),
+            Rule::EOI => (),
+            _ => println!("Unknown rule: {:?}", pair.as_rule()),
+        }
+    }
+}
+
+fn process_var_declaration(pairs: pest::iterators::Pairs<Rule>) {
+    let mut identifier = String::new();
+    let mut expression = String::new();
+
+    for pair in pairs {
+        match pair.as_rule() {
+            Rule::IDENTIFIER => identifier = pair.as_str().to_string(),
+            Rule::EXPRESSION => expression = pair.as_str().to_string(),
+            Rule::DECLARATION_WORD => println!("Begin declaration:"),
+            Rule::EQUALS => (),
+            Rule::SEMICOLON => println!("End declaration"),
+            _ => println!("{}", pair.as_str().to_string()),
+        }
+    }
+
+    println!("Variable: {}", identifier);
+    println!("Value: {}", expression);
 }
